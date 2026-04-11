194cf3286abb2de2275f0fcea91b56521004b111
rdsemon <codewithemons@gmail.com>
2026-04-11 22:13:56 +0600

    refactor:remove the test function

--- src/controllers/initail.controller.ts
+++ src/controllers/initail.controller.ts
@@ -3,7 +3,3 @@ import type { Request, Response } from "express";
 export const hello = (req: Request, res: Response) => {
   res.send("Hello from the server");
 };
-
-export const hi = (req: Request, res: Response) => {
-  res.send("Hi");
-};
