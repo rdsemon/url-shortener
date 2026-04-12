5c588ac664308513db2b21ea159ca08ce886313f
rdsemon <codewithemons@gmail.com>
2026-04-12 22:54:05 +0600

    feat(user.model.ts):create users table

---
+++ src/models/user.model.ts
@@ -0,0 +1,18 @@
+import {
+  integer,
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  text,
+} from "drizzle-orm/pg-core";
+
+const usersTable = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 90 }).notNull(),
+  email: varchar("email", { length: 150 }).unique().notNull(),
+  hashPassword: text("hash_Password").notNull(),
+  createdAt: timestamp("created_At").defaultNow().notNull(),
+});
+
+export default usersTable;
