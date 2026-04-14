c46a9f2d45d2aaacba5bc3e201ddb2b95a56bd09
rdsemon <codewithemons@gmail.com>
2026-04-14 19:34:47 +0600

    connnet the url route and limit json data size

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import initialRoute from "./routes/initail.route.ts";
+import urlRoute from "./routes/url.routes.ts";
 
 const app = express();
+app.use(express.json({ limit: "100kb" }));
 
-app.use(initialRoute);
+app.use("/api/v1", urlRoute);
 
 export default app;
