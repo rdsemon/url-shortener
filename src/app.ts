da3d705ebbbdd46a1801a2b8cc6846548ac5e6ca
rdsemon <codewithemons@gmail.com>
2026-04-14 19:43:43 +0600

    feat(app.ts): connect the authRoute

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import urlRoute from "./routes/url.routes.ts";
+import authRoute from "./routes/auth.routes.ts";
 
 const app = express();
 app.use(express.json({ limit: "100kb" }));
 
 app.use("/api/v1", urlRoute);
+app.use("/api/v1", authRoute);
 
 export default app;
