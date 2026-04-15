22d89d4160bd709c83acff4360e7216b1707ab03
rdsemon <codewithemons@gmail.com>
2026-04-15 22:43:27 +0600

    refactor(url): use RequestHandler instead of explicit req, res, next types

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,10 +1,6 @@
 import { testApi } from "../utils/apiTester.ts";
-import type { Request, Response, NextFunction } from "express";
-const createShortUrl = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+import type { RequestHandler } from "express";
+const createShortUrl: RequestHandler = async (req, res, next) => {
   console.log(req.body);
 
   testApi(res);
