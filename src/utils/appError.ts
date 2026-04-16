a165267ef359ba0bfb54cbbe1732f846bfb46723
rdsemon <codewithemons@gmail.com>
2026-04-16 15:59:04 +0600

    refactor(appError): add two new options message and status

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,10 +1,14 @@
 class AppError extends Error {
   statusCode: number;
   isOperational: true;
+  status: string;
   constructor(message: string, statusCode: number) {
     super(message);
 
+    this.message = message;
     this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
