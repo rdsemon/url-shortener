4934cbdf672f600cfc0c91c06ef2de66b850e23c
rdsemon <codewithemons@gmail.com>
2026-04-16 15:39:46 +0600

    feat(app):connect the handleInvalidRoute middleware fn

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import urlRoute from "./routes/url.routes.ts";
 import authRoute from "./routes/auth.routes.ts";
+import hanldeInvallidRoute from "./utils/invalidRoute.ts";
 
 const app = express();
 app.use(express.json({ limit: "100kb" }));
 
 app.use("/api/v1", urlRoute);
 app.use("/api/v1", authRoute);
+
+app.use(hanldeInvallidRoute);
 
 export default app;
