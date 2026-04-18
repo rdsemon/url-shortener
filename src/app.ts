eaa6a0ead460977c05a2524e64e564e6344f8d25
rdsemon <codewithemons@gmail.com>
2026-04-18 22:11:41 +0600

    feat(app):connect the handleGlobalError controller function

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import urlRoute from "./routes/url.routes.ts";
 import authRoute from "./routes/auth.routes.ts";
-import hanldeInvallidRoute from "./utils/invalidRoute.ts";
+import hanldeInvallidRoute from "./middlewares/invalidRoute.ts";
+import handleGlobalError from "./controllers/error.controller.ts";
 
 const app = express();
 app.use(express.json({ limit: "100kb" }));
@@ -10,5 +11,6 @@ app.use("/api/v1", urlRoute);
 app.use("/api/v1", authRoute);
 
 app.use(hanldeInvallidRoute);
+app.use(handleGlobalError);
 
 export default app;
