847d5d8f6c1e56e5f843821a782fe9a1ec3c6203
rdsemon <codewithemons@gmail.com>
2026-04-19 23:27:51 +0600

    feat(app):connect the logger middleware

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import urlRoute from "./routes/url.routes.ts";
 import authRoute from "./routes/auth.routes.ts";
 import hanldeInvallidRoute from "./middlewares/invalidRoute.ts";
 import handleGlobalError from "./controllers/error.controller.ts";
+import { loger } from "./middlewares/logger.ts";
 
 const app = express();
 app.use(express.json({ limit: "100kb" }));
+
+app.use(loger);
 
 app.use("/api/v1", urlRoute);
 app.use("/api/v1", authRoute);
