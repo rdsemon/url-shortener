cd924a01b7c0008f7151e92b4823315c9d7914e4
rdsemon <codewithemons@gmail.com>
2026-04-20 21:08:44 +0600

    refactor(auth): change the route path in login and signup

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import validateInput from "../middlewares/zodValidator.ts";
 import { singUpSchema } from "../zodSchema/auth.schema.ts";
 const router = express.Router();
 
-router.post("/auth", validateInput(singUpSchema), signUp);
-router.post("/auth", login);
+router.post("/auth/signUp", validateInput(singUpSchema), signUp);
+router.post("/auth/login", login);
 
 export default router;
