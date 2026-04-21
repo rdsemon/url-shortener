7a21ac7695a4ed49b0c1c86f650d513474968b69
rdsemon <codewithemons@gmail.com>
2026-04-21 17:19:07 +0600

    feat(auth.routes.ts):validate login inputs data

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { login, signUp } from "../controllers/auth.controller.ts";
 import validateInput from "../middlewares/zodValidator.ts";
-import { singUpSchema } from "../zodSchema/auth.schema.ts";
+import { loginSchema, singUpSchema } from "../zodSchema/auth.schema.ts";
 const router = express.Router();
 
 router.post("/auth/signUp", validateInput(singUpSchema), signUp);
-router.post("/auth/login", login);
+router.post("/auth/login", validateInput(loginSchema), login);
 
 export default router;
