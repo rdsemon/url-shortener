c450203773199a241e82984ad91991c5b7716560
rdsemon <codewithemons@gmail.com>
2026-04-21 17:13:38 +0600

    feat(sendJwt):send the jwt token

---
+++ src/utils/sendJwt.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import type { Response } from "express";
+
+export const sendJwt = (userId: string, res: Response) => {
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) throw new Error("JWT_SECRET is not defined");
+
+  const expiresIn = process.env.JWT_EXPIRE_IN ?? "10m";
+
+  // @ts-expect-error: jsonwebtoken overload issue with expiresIn type
+
+  const token = jwt.sign({ id: userId }, jwtSecret, { expiresIn });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 10 * 60 * 1000,
+  });
+
+  return token;
+};
