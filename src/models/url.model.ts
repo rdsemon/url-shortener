e62e59b8b39eb36c31548662e400f693c245f123
rdsemon <codewithemons@gmail.com>
2026-04-24 21:54:05 +0600

    refactor(url.model.ts): decrease the shortUrl length from 150 to 50 cheracter

--- src/models/url.model.ts
+++ src/models/url.model.ts
@@ -11,7 +11,7 @@ const urlsTable = pgTable("url", {
   id: uuid("id").defaultRandom().primaryKey(),
   originalUrl: text("original_Url").notNull(),
   shortCode: varchar("short_code", { length: 10 }).notNull().unique(),
-  shortUrl: varchar("short_Url", { length: 150 }).notNull().unique(),
+  shortUrl: varchar("short_Url", { length: 50 }).notNull().unique(),
   createdAt: timestamp("created_At").defaultNow().notNull(),
   clickCount: integer("click_Count").default(0).notNull(),
 });
