b01e7608bf42fe5fb13ee8cf660336ccdec10df0
rdsemon <codewithemons@gmail.com>
2026-04-24 22:11:40 +0600

    feat(url.routes.ts):create get url route

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createShortUrl } from "../controllers/url.controller.ts";
+import { createShortUrl, getUrl } from "../controllers/url.controller.ts";
 import validateInput from "../middlewares/zodValidator.ts";
 import { urlSchema } from "../zodSchema/url.schema.ts";
 import { protect } from "../controllers/auth.controller.ts";
 const router = express.Router();
 
-router.route("/url").post(validateInput(urlSchema), createShortUrl);
+router.route("/shorten").post(validateInput(urlSchema), createShortUrl);
+router.route("/:shortCode").get(getUrl);
 
 export default router;
