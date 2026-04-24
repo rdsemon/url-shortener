301f6367aa83d3193c0bd73ad4347c6c6c74baea
rdsemon <codewithemons@gmail.com>
2026-04-24 21:51:25 +0600

    refactor(url.routes.ts):use validateInput middleware for validate input according to zod schema

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { createShortUrl } from "../controllers/url.controller.ts";
+import validateInput from "../middlewares/zodValidator.ts";
+import { urlSchema } from "../zodSchema/url.schema.ts";
+import { protect } from "../controllers/auth.controller.ts";
 const router = express.Router();
 
-router.route("/url").post(createShortUrl);
+router.route("/url").post(validateInput(urlSchema), createShortUrl);
 
 export default router;
