5fd1198afa96ef486d8a9f797347857e4a72345c
rdsemon <codewithemons@gmail.com>
2026-04-24 22:12:44 +0600

    feat(url.controller.ts): create get url controller function

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,9 +1,19 @@
 import { testApi } from "../utils/apiTester.ts";
-import type { RequestHandler } from "express";
-const createShortUrl: RequestHandler = async (req, res, next) => {
-  console.log(req.body);
+import AppError from "../utils/appError.ts";
+import asyncHandler from "../utils/catchAsyncErrorHandler.ts";
+
+const createShortUrl = asyncHandler(async (req, res, next) => {
+  const { originalUrl } = req.body;
+
+  if (originalUrl.length < 50)
+    return next(new AppError("Url is alreay short", 400));
+
+  console.log(originalUrl.length);
 
   testApi(res);
-};
+});
 
-export { createShortUrl };
+const getUrl = asyncHandler(async (req, res, next) => {
+  testApi(res);
+});
+export { createShortUrl, getUrl };
