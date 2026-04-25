ee3490bd6b03d4b97f338dff60415676c4ac2ee0
rdsemon <codewithemons@gmail.com>
2026-04-25 23:07:51 +0600

    feat(url.controller.ts):implement createShortUrl and getUrlByCode handler functions

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,6 +1,10 @@
-import { testApi } from "../utils/apiTester.ts";
-import AppError from "../utils/appError.ts";
 import asyncHandler from "../utils/catchAsyncErrorHandler.ts";
+import generateShortUrl from "../utils/createShortUrl.ts";
+import urlsTable from "../models/url.model.ts";
+import AppError from "../utils/appError.ts";
+import db from "../database.ts";
+import { updateClickCount } from "../services/db.services.ts";
+import { eq } from "drizzle-orm";
 
 const createShortUrl = asyncHandler(async (req, res, next) => {
   const { originalUrl } = req.body;
@@ -8,12 +12,44 @@ const createShortUrl = asyncHandler(async (req, res, next) => {
   if (originalUrl.length < 50)
     return next(new AppError("Url is alreay short", 400));
 
-  console.log(originalUrl.length);
+  const urlData = generateShortUrl(originalUrl);
 
-  testApi(res);
+  if (!urlData) return next(new AppError("url data genarating fail", 400));
+
+  const [url] = await db
+    .insert(urlsTable)
+    .values(urlData)
+    .returning({ shortUrl: urlsTable.shortUrl });
+
+  if (!url) return next(new AppError("Url creation fail", 400));
+
+  res.status(201).json({ status: "successful", id: url.shortUrl });
 });
 
-const getUrl = asyncHandler(async (req, res, next) => {
-  testApi(res);
+const getUrlByCode = asyncHandler(async (req, res, next) => {
+  const shortCode = req.params.shortCode as string;
+
+  if (!shortCode) return next(new AppError("code not found", 400));
+
+  // get the url from database
+  const [url] = await db
+    .select({
+      originalUrl: urlsTable.originalUrl,
+      clickCount: urlsTable.clickCount,
+      id: urlsTable.id,
+    })
+    .from(urlsTable)
+    .where(eq(urlsTable.shortCode, shortCode));
+
+  if (!url) {
+    return res.status(404).json({ message: "URL not found" });
+  }
+
+  const { clickCount, id } = url;
+
+  //update the clickCount
+  await updateClickCount(clickCount, id);
+
+  res.redirect(url.originalUrl);
 });
-export { createShortUrl, getUrl };
+export { createShortUrl, getUrlByCode };
