95172af271ea5978a327929ca8e7ad65df392274
rdsemon <codewithemons@gmail.com>
2026-04-25 23:14:17 +0600

    feat(createShortUrl):create new helper function to generate short url and return object

---
+++ src/utils/createShortUrl.ts
@@ -0,0 +1,21 @@
+import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
+
+const generateShortUrl = (url: string) => {
+  const nanoid = customAlphabet(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    7,
+  );
+  if (url) {
+    const shortCode = nanoid();
+    const shortUrl = `http://localhost:${process.env.PORT}/api/v1/${shortCode}`;
+
+    return {
+      originalUrl: url,
+      shortCode,
+      shortUrl,
+    };
+  }
+};
+
+export default generateShortUrl;
