50b938ffb733d2642b2696ebfcb033026f51f58f
rdsemon <codewithemons@gmail.com>
2026-04-25 23:12:17 +0600

    refactor(url.routes.ts):change getUrl route name to getUrlByCode

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { createShortUrl, getUrl } from "../controllers/url.controller.ts";
+import { createShortUrl, getUrlByCode } from "../controllers/url.controller.ts";
 import validateInput from "../middlewares/zodValidator.ts";
 import { urlSchema } from "../zodSchema/url.schema.ts";
 import { protect } from "../controllers/auth.controller.ts";
 const router = express.Router();
 
 router.route("/shorten").post(validateInput(urlSchema), createShortUrl);
-router.route("/:shortCode").get(getUrl);
+router.route("/:shortCode").get(getUrlByCode);
 
 export default router;
