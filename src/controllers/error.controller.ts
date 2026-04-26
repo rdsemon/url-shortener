7ce49c894d7ed4c1d57cdcd5a20c72d6a8be2ed8
rdsemon <codewithemons@gmail.com>
2026-04-26 22:50:03 +0600

    feat:new error handler fucntion to send error in production and development mode

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,5 +1,31 @@
 import type { Request, Response, NextFunction } from "express";
 
+const sendErrorDev = (err: any, res: Response, statusCode: number) => {
+  res.status(statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stackTrace,
+    message: err.message,
+  });
+};
+
+const duplicateKeyError = (err: any) => {
+  const detail = err?.detail;
+
+  const match = detail?.match(/\((.*?)\)=\((.*?)\)/);
+
+  const value = match?.[2] ?? "Value";
+
+  const message = `${value} already exists`;
+  return message;
+};
+
+const sendErrorProd = (err: any, res: Response, statusCode: number) => {
+  res.status(statusCode).json({ status: err.status, message: err });
+};
+
+console.log(process.env.NODE_ENV);
+
 const handleGlobalError = (
   err: any,
   req: Request,
@@ -7,7 +33,20 @@ const handleGlobalError = (
   next: NextFunction,
 ) => {
   const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({ status: err.status, message: err.message });
+
+  if (process.env.NODE_ENV === "production") {
+    sendErrorDev(err, res, statusCode);
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    console.log(err);
+    let error = err;
+    if (error.cause.code === "23505") {
+      error = duplicateKeyError(error);
+    }
+    console.log(error);
+    sendErrorProd(error, res, statusCode);
+  }
 };
 
 export default handleGlobalError;
