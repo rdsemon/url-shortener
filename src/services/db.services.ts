88299ac13c4d5048814309235f1dec1cbc0a70b8
rdsemon <codewithemons@gmail.com>
2026-04-26 22:54:52 +0600

    fix: change import extensions from .ts to .js

--- src/services/db.services.ts
+++ src/services/db.services.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
-import db from "../database.ts";
-import urlsTable from "../models/url.model.ts";
+import db from "../database.js";
+import urlsTable from "../models/url.model.js";
 
 export const updateClickCount = async (clickCount: number, urlId: string) => {
   clickCount++;
