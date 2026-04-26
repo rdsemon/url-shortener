9a81b141e4e82000441ad4dc78c97e695093357d
rdsemon <codewithemons@gmail.com>
2026-04-26 22:53:35 +0600

    fix: change import extensions from .ts to .js

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { createShortUrl, getUrlByCode } from "../controllers/url.controller.ts";
-import validateInput from "../middlewares/zodValidator.ts";
-import { urlSchema } from "../zodSchema/url.schema.ts";
-import { protect } from "../controllers/auth.controller.ts";
+import { createShortUrl, getUrlByCode } from "../controllers/url.controller.js";
+import validateInput from "../middlewares/zodValidator.js";
+import { urlSchema, urlParamsSchema } from "../zodSchema/url.schema.js";
+import { protect } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.route("/shorten").post(validateInput(urlSchema), createShortUrl);
-router.route("/:shortCode").get(getUrlByCode);
+router
+  .route("/shorten")
+  .post(validateInput(urlSchema), protect, createShortUrl);
+router.route("/:shortCode").get(validateInput(urlParamsSchema), getUrlByCode);
 
 export default router;
