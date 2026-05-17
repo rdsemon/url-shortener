a18070bd1eed0363295be81307f359a3194f4037
rdsemon <codewithemons@gmail.com>
2026-05-17 22:40:14 +0600

    refactor(url.controller):replace the db logic from here

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,10 +1,11 @@
 import asyncHandler from "../utils/catchAsyncErrorHandler.js";
 import generateShortUrl from "../utils/createShortUrl.js";
-import urlsTable from "../models/url.model.js";
 import AppError from "../utils/appError.js";
-import db from "../database.js";
 import { updateClickCount } from "../services/db.services.js";
-import { eq } from "drizzle-orm";
+import {
+  createUrlService,
+  getUrlByCodeService,
+} from "../service/dbService/urlDb.service.js";
 
 const createShortUrl = asyncHandler(async (req, res, next) => {
   const { originalUrl } = req.body;
@@ -16,14 +17,9 @@ const createShortUrl = asyncHandler(async (req, res, next) => {
 
   if (!urlData) return next(new AppError("url data genarating fail", 400));
 
-  const [url] = await db
-    .insert(urlsTable)
-    .values(urlData)
-    .returning({ shortUrl: urlsTable.shortUrl });
+  const url = await createUrlService(urlData);
 
-  if (!url) return next(new AppError("Url creation fail", 400));
-
-  res.status(201).json({ status: "successful", id: url.shortUrl });
+  res.status(201).json({ status: "successful", shortUrl: url.shortUrl });
 });
 
 const getUrlByCode = asyncHandler(async (req, res, next) => {
@@ -31,19 +27,7 @@ const getUrlByCode = asyncHandler(async (req, res, next) => {
 
   if (!shortCode) return next(new AppError("code not found", 400));
 
-  // get the url from database
-  const [url] = await db
-    .select({
-      originalUrl: urlsTable.originalUrl,
-      clickCount: urlsTable.clickCount,
-      id: urlsTable.id,
-    })
-    .from(urlsTable)
-    .where(eq(urlsTable.shortCode, shortCode));
-
-  if (!url) {
-    return res.status(404).json({ message: "URL not found" });
-  }
+  const url = await getUrlByCodeService(shortCode);
 
   const { clickCount, id } = url;
   //update the clickCount
